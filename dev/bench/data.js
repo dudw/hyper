e89c3c06dc2b17231eca48fe02da16367afdff38
github-action-benchmark <github@users.noreply.github.com>
2026-06-22 22:31:14 +0000

    add pipeline (cargo) benchmark result for 2e32de6ed15b5cb57ab9275444fa42eb4e3b5870

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781886826560,
+  "lastUpdate": 1782167474059,
   "repoUrl": "https://github.com/dudw/hyper",
   "entries": {
     "connect": [
@@ -8131,6 +8131,36 @@ window.BENCHMARK_DATA = {
             "name": "hello_world_16",
             "value": 57306,
             "range": "± 10767.60",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "128471892+nakaryo716@users.noreply.github.com",
+            "name": "Nakaryo",
+            "username": "nakaryo716"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2e32de6ed15b5cb57ab9275444fa42eb4e3b5870",
+          "message": "style(lib): remove `default_trait_access` lint (#4111)\n\nPart of #4071",
+          "timestamp": "2026-06-22T10:19:16-04:00",
+          "tree_id": "d7dc44ef9b9aa362b127002b5a7ec44bf27cedc1",
+          "url": "https://github.com/dudw/hyper/commit/2e32de6ed15b5cb57ab9275444fa42eb4e3b5870"
+        },
+        "date": 1782167471473,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "hello_world_16",
+            "value": 44486,
+            "range": "± 6792.70",
             "unit": "ns/iter"
           }
         ]
