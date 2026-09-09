7823596045ea480a98ed93538345d1d3f1c74112
github-action-benchmark <github@users.noreply.github.com>
2026-09-09 16:33:37 +0000

    add end_to_end (cargo) benchmark result for 6612d993a6bcfb6a21a8739b51779a30170402cc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788971484718,
+  "lastUpdate": 1788971617777,
   "repoUrl": "https://github.com/dudw/hyper",
   "entries": {
     "connect": [
@@ -32127,6 +32127,114 @@ window.BENCHMARK_DATA = {
             "name": "http2_parallel_x10_res_1mb",
             "value": 5613379,
             "range": "± 88196.76",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean@seanmonstar.com",
+            "name": "Sean McArthur",
+            "username": "seanmonstar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6612d993a6bcfb6a21a8739b51779a30170402cc",
+          "message": "docs(maintainers): add 0x676e67 as a collaborator (#4184)",
+          "timestamp": "2026-09-09T08:06:03-04:00",
+          "tree_id": "8472831d758f48de07162283d7053ac474056f55",
+          "url": "https://github.com/dudw/hyper/commit/6612d993a6bcfb6a21a8739b51779a30170402cc"
+        },
+        "date": 1788971616381,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "http1_consecutive_x1_both_100kb",
+            "value": 66055,
+            "range": "± 1687.35",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http1_consecutive_x1_both_10mb",
+            "value": 4072751,
+            "range": "± 347213.68",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http1_consecutive_x1_empty",
+            "value": 20740,
+            "range": "± 4149.82",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http1_consecutive_x1_req_10b",
+            "value": 22315,
+            "range": "± 4878.04",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_consecutive_x1_empty",
+            "value": 29473,
+            "range": "± 2689.30",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_consecutive_x1_req_100kb",
+            "value": 98762,
+            "range": "± 13125.92",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_consecutive_x1_req_10b",
+            "value": 40999877,
+            "range": "± 7955.45",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_empty",
+            "value": 79729,
+            "range": "± 2056.10",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_req_10kb_100_chunks",
+            "value": 16314227,
+            "range": "± 16477044.89",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_req_10kb_100_chunks_adaptive_window",
+            "value": 7964955,
+            "range": "± 140626.44",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_req_10kb_100_chunks_max_window",
+            "value": 8015855,
+            "range": "± 227459.92",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_req_10mb",
+            "value": 95404894,
+            "range": "± 1002515.36",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_res_10mb",
+            "value": 54542494,
+            "range": "± 708852.77",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "http2_parallel_x10_res_1mb",
+            "value": 5523546,
+            "range": "± 92909.17",
             "unit": "ns/iter"
           }
         ]
